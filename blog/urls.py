cd10d807d0561202904cf18b45914942540dfa98
Carlos Pimentel <capimentel64@gmail.com>
2024-08-22 23:11:17 +0000

    Finish JavaScript intro and fundamentals

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # other patterns
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
     path("ip/", blog.views.get_ip)
 ]
 
